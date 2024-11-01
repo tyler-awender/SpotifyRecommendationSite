e2f4f0a558bd143a8ba7b0d4549d6d5443cf21a2
mckenzielola <mckenzielola12@yahoo.com>
2024-11-01 14:51:06 -0700

    implemented home to have links for other pages

--- SpotifyClient/oauth.py
+++ SpotifyClient/oauth.py
@@ -10,6 +10,7 @@ from requests import post, get # to allow for post requests
 # store client_id and client_secret from env file
 # client_id = os.getenv("CLIENT_ID")
 # client_secret = os.getenv("CLIENT_SECRET")
+
 client_id = "f4968da54d444c79b4f9296ba647de85"
 client_secret = "755f3f594b074d1fb6a7cf8758fd0646"
 
